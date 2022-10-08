24d38f8207107d1fe5773b2240118674b61c2a6a
syuan0902 <syuan7492@icloud.com>
2022-10-09 02:39:13 +0800

    第一次commit

---
+++ SpringBoot/src/main/java/com/syuan/Application.java
@@ -0,0 +1,17 @@
+package com.syuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot的啟動器
+ * @author syuan
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
